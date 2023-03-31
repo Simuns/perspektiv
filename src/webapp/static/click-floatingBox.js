c9c3a6cb420a4e188d80c87c4cea8a469a87be42
simuns <simunhojgaard@gmail.com>
2023-03-31 08:39:54 +0200

    made articles expandable, and closable

--- src/webapp/static/click-floatingBox.js
+++ src/webapp/static/click-floatingBox.js
@@ -1,16 +1,44 @@
-// Declare the variable outside the function to ensure it's only declared once
 const floatingBoxes = document.querySelectorAll('.floatingBox');
 
-// Loop through each of the floatingBox div elements
 floatingBoxes.forEach(box => {
-  // Add a click event listener to each box
-  box.addEventListener('click', () => {
-    // Get the box's ID
-    const boxId = box.id;
+  // Define the function to restore the original content
+  function restoreOriginalContent() {
+    box.innerHTML = box.getAttribute('data-content');
+    box.classList.remove('open');
+  }
 
-    // Redirect to the URL with the box ID appended as a parameter
-    window.location.href = `/brøv/${boxId}`;
+  box.addEventListener('click', () => {
+    // Check if the box is already open
+    if (box.classList.contains('open')) {
+      restoreOriginalContent();
+      return;
+    }
+
+    const boxId = box.id;
+    fetch(`/brøv/${boxId}`)
+      .then(response => response.text())
+      .then(html => {
+        // Parse the HTML string
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+
+        // Add the link to the CSS file
+        const cssLink = doc.createElement('link');
+        cssLink.setAttribute('rel', 'stylesheet');
+        cssLink.setAttribute('href', '/static/article.css');
+        doc.head.appendChild(cssLink);
+
+        // Get the desired element
+        const boxContent = doc.querySelector('.art');
+
+        // Replace the box content with the new content
+        box.setAttribute('data-content', box.innerHTML);
+        box.innerHTML = boxContent.innerHTML;
+
+        // Add the "open" class to the box
+        box.classList.add('open');
+      });
   });
-    // Add a pointer cursor when hovering over the box
-    box.style.cursor = 'pointer';
+
+  box.style.cursor = 'pointer';
 });

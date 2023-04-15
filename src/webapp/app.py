489d28070ed3f373a2d5dbe3b11b654a799a533d
simuns <simunhojgaard@gmail.com>
2023-04-16 01:46:11 +0200

    moved old verification logic to own file, and reimplemented it

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -2,28 +2,34 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, send_from_directory
 #loading settings defined in the settings.yaml file
 from webapp.settings import app_config
+# handling of verification
+# database dependencies
+from webapp.database import db, artiklar, Verification, UserModel, login
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc, text
+
 # Used for creating id's for articles
 import uuid
+
 #datetime for timestampings
 from datetime import datetime, timedelta
-# Verify phone number with email sending
-import sendgrid
+
 import os
-from sendgrid.helpers.mail import Mail, Email, To, Content
-# generate verification code
-import random
+
 # Used for picture reduction in size
 from PIL import Image
-from webapp.database import db, artiklar, UserModel, login
+
+# handle login events
 from flask_login import login_required, current_user, login_user, logout_user
 # Load settings defined in config.yaml
-app_config = app_config()
 #regex
 import re
 
 app = Flask(__name__)
+#importing webapp after initialization of app
+from webapp.verify import send_verification, verify
+
+app_config = app_config()
 # THE SECRET IS USED FOR CREATING CLIENT SESSIONS AND ENCRYPTING THEM
 app.secret_key = app_config['secret_key']
 # Get the current working directory
@@ -63,7 +69,7 @@ def index():
     if article_count == 0:
         session_id = str(uuid.uuid4())[:8]
         return render_template('skriva.html', session_id=session_id)
-    seinastu_artiklar_dbRaw = artiklar.query.filter_by(verified=True).order_by(artiklar.created_stamp.desc()).limit(10).all()
+    seinastu_artiklar_dbRaw = artiklar.query.join(Verification).filter(Verification.status == 'verified').order_by(artiklar.created_stamp.desc()).limit(10).all()
     seinastu_artiklar_dict = latest_articles_dict(seinastu_artiklar_dbRaw)
     for article in seinastu_artiklar_dict:
         time_delta = timeDelta(seinastu_artiklar_dict[article]["created_stamp"])
@@ -106,16 +112,13 @@ def index_loadMore():
 
 @app.route('/brøv/<string:article_id>')
 def show_article(article_id):
-    art = artiklar.query.filter_by(art_id=article_id).first()
+    art = artiklar.query.join(Verification).filter(artiklar.art_id == article_id, Verification.status == 'verified').first()
     if art is None:
         return render_template('error.html',error="Brævið finst ikki")
-    if art.verified:
-        art_dict = rowToDict(art)
-        date_string = art_dict["created_stamp"].strftime('%Y-%m-%d')
-        art_dict["date_string"] = date_string
-        return render_template('article.html',art_dict=art_dict)
-    else:
-        return render_template('error.html',error="Brævið er ikki váttað enn")
+    art_dict = rowToDict(art)
+    date_string = art_dict["created_stamp"].strftime('%Y-%m-%d')
+    art_dict["date_string"] = date_string
+    return render_template('article.html',art_dict=art_dict)
 
 @app.route('/skriva', methods=['POST', 'GET'])
 def skriva():
@@ -127,11 +130,7 @@ def skriva():
         telefon = request.form['telefon']
         yvirskrift = request.form['yvirskrift']
         quill_data = request.form.get('text')
-        print("request form",request.form)
-        
-        if app_config["verifyPhone"]:
-            #generate code
-            code = random.randint(100000, 999999)
+
         art = artiklar.query.filter_by(art_id=session_id).first()
         if art:
             print("USER FOUND WITH SESSION ID:",session_id,"updating user")
@@ -142,11 +141,7 @@ def skriva():
             art.yvirskrift=yvirskrift
             art.skriv=quill_data
             art.created_stamp = datetime.utcnow()
-            if app_config["verifyPhone"]:
-                art.vercode=code
-            else:
-                art.verified=True
-            db.session.commit()
+
         else:
             print("NO USER FOUND WITH USER ID",session_id)
             nytt_skriv = artiklar(
@@ -158,15 +153,13 @@ def skriva():
                 yvirskrift=yvirskrift,
                 skriv=quill_data,
                 created_stamp = datetime.utcnow(),
-                vercode = code if app_config["verifyPhone"] else None,
-                verified = False if app_config["verifyPhone"] else True
                 )
 
             db.session.add(nytt_skriv)
             db.session.commit()
 
         if app_config["verifyPhone"]:
-            verifyPhone(app_config, telefon, code)
+            send_verification(session_id, "article", "sms", telefon)
             return render_template('verify.html', session_id=session_id, telefon=telefon)
         else:
             return redirect(url_for('index'))
@@ -233,18 +226,13 @@ def verify_status():
     if request.method == 'POST':
         session_id = request.form['session_id']
         verified_code = request.form['verification_code']
-        art = artiklar.query.filter_by(art_id=session_id).first()
-        if verified_code == art.vercode:
-            if art:
-                print("USER FOUND WITH SESSION ID:",session_id,"updating verified status")
-                art.verified=True
-                db.session.commit()
-
+        verifyStatus = verify(session_id, "sms", verified_code)
+        if verifyStatus == "verified":
             return render_template('verify_success.html')
         else:
+            art = artiklar.query.filter_by(art_id=session_id).first()
             return render_template('verify.html', session_id=session_id, telefon=art.telefon, status="Kodan var skeiv, prøva umaftur!")
-
-@app.route('/register', methods=['POST', 'GET'])
+app.route('/register', methods=['POST', 'GET'])
 def register():
     if current_user.is_authenticated:
         return redirect('/profilur')
@@ -270,6 +258,7 @@ def register():
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
+
         return redirect('/ritainn')
     return render_template('register.html')
 
@@ -305,7 +294,7 @@ def send_sms():
             data = request.get_json()
             session_id = data.get('session_id')
             art = artiklar.query.filter_by(id=session_id).first()
-            verifyPhone(app_config, art.telefon, art.vercode)
+            send_verification(session_id, "article", "sms", art.telefon)
             return jsonify({'success': True}), 200
         else:
             return jsonify({'Phone number sms verification not activated': True}), 500
@@ -406,24 +395,7 @@ def inject_auth():
     return {'authenticated': False}
 
 
-## USED TO SEND SMS's TO THE END USER ##
-# THIS IS ACHIEVED BY FIRST SENDING MAIL TO MAIL->SMS RELAY #
-def verifyPhone(app_config, phoneNumber, code):
-    if app_config['verifyPhone']:
-        sg = sendgrid.SendGridAPIClient(api_key=app_config['verify_apiKey'])
-        from_email = Email(app_config['verify_senderMail'])  # Change to your verified sender
-        to_email = To(phoneNumber+'@'+app_config['verify_receiver'])  # Change to your recipient
-        subject = "SMS FRÁ perspektiv.fo"
-        content = Content("text/plain", f"Vátta við kodu: {code}")
-        mail = Mail(from_email, to_email, subject, content)
-
-        # Get a JSON-ready representation of the Mail object
-        mail_json = mail.get()
-
-        # Send an HTTP POST request to /mail/send
-        response = sg.client.mail.send.post(request_body=mail_json)
-        print(response.status_code)
-        print(response.headers)
+print (verify(12345678, "email", "955786"))
 
 
 ## KEEP APP RUNNING ##

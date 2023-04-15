908ff5a4c92e7aaa0d7a5139fd05396a2c0b9cf1
simuns <simunhojgaard@gmail.com>
2023-04-16 01:45:19 +0200

    added verification table to database

--- src/webapp/database.py
+++ src/webapp/database.py
@@ -20,8 +20,8 @@ class artiklar(db.Model):
     skriv = db.Column(db.Text)
     picture_path = db.Column(db.String(length=120), nullable=True)
     created_stamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
-    verified = db.Column(db.Boolean, default=False)
-    vercode = db.Column(db.String(6), nullable=True)
+
+    verify = db.relationship("Verification", back_populates="article")
 
 
 ## DEFINE USER TABLE
@@ -34,13 +34,31 @@ class UserModel(UserMixin, db.Model):
     efturnavn = db.Column(db.String(50), nullable=True)
     telefon = db.Column(db.String(6), nullable=True)
     password_hash = db.Column(db.String())
+    created_stamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
+    
+    verify = db.relationship("Verification", back_populates="user")
+
 
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
+
+class Verification(db.Model):
+    __tablename__ = 'verify'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code_sms = db.Column(db.String)
+    code_email = db.Column(db.String)
+    method = db.Column(db.String)
+    status = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    article_id = db.Column(db.String, db.ForeignKey('artiklar.art_id'), nullable=True)
     
+    user = db.relationship("UserModel", back_populates="verify")
+    article = db.relationship("artiklar", back_populates="verify")
 
 
 from flask_login import LoginManager
@@ -49,3 +67,5 @@ login = LoginManager()
 @login.user_loader
 def load_user(id):
     return UserModel.query.get(int(id))
+
+
